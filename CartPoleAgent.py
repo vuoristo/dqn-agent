17bcdb06422b30df2c373f3554d2a5b39ad36b94
Risto Vuorio <vuoristo@gmail.com>
2016-09-21 08:37:45 +0300

    soft updates to CartPole

--- CartPoleAgent.py
+++ CartPoleAgent.py
@@ -6,7 +6,7 @@ from DQNAgent import DQNAgent
 ENV_NAME = 'CartPole-v0'
 def main():
   env = gym.make(ENV_NAME)
-  model = SimpleDQNModel(env)
+  model = SimpleDQNModel(env, soft_updates=True)
   agent = DQNAgent(env, model)
   agent.train()
 
