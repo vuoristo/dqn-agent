7782d11e342db0aa546a2dcd803b1e30cb4f2428
Risto Vuorio <vuoristo@gmail.com>
2016-09-27 15:11:48 +0300

    Update CartPoleAgent to use DNNModel

--- CartPoleAgent.py
+++ CartPoleAgent.py
@@ -1,13 +1,17 @@
 import gym
 
-from SimpleDQNModel import SimpleDQNModel
+from DNNModel import DNNModel
 from DQNAgent import DQNAgent
 
 ENV_NAME = 'CartPole-v0'
 def main():
   env = gym.make(ENV_NAME)
-  model = SimpleDQNModel(env, soft_updates=True)
-  agent = DQNAgent(env, model)
+  model = DNNModel(env, initial_learning_rate=0.1,
+                   gamma=0.99, hidden_1=10, hidden_2=6,
+                   soft_updates=True)
+  agent = DQNAgent(env, model, epsilon=0.9,
+                   epsilon_decay=0.97,
+                   exp_buffer_size=40000, batch_size=50)
   agent.train()
 
 if __name__ == '__main__':
