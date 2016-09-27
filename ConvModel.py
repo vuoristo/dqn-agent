e3bcab20406735aa899e97b88a517bb7ae3ad595
Risto Vuorio <vuoristo@gmail.com>
2016-09-27 11:20:12 +0300

    Adds grayscale flag to ConvModel. Better layer shaping

--- ConvModel.py
+++ ConvModel.py
@@ -20,22 +20,28 @@ def max_pool_2x2(x):
   return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
 class ConvModel(DQNModel):
-  def __init__(self, env, resize_shape=(84, 84),
-              window_size=4, **kwargs):
+  def __init__(self, env, resize_shape=(64, 64),
+              window_size=4, grayscale=True, **kwargs):
     self.resize_shape = resize_shape
     self.input_shape = list(env.observation_space.shape)
     self.input_shape[0] = resize_shape[0]
     self.input_shape[1] = resize_shape[1]
     self.input_shape[2] *= window_size
+
+    self.grayscale = grayscale
+    if grayscale:
+      self.input_shape[2] = window_size
+
     self.window_size = window_size
     super(ConvModel, self).__init__(env, **kwargs)
 
   def build_net(self, x):
-    """Builds a two layer neural network. Returns a dictionary
-    containing weight variables and outputs.
+    """Builds a convolutional neural network. Assumes square
+    input images. Returns a dictionary containing weight
+    variables and outputs.
     """
 
-    W_conv1 = weight_variable([5,5,12,32])
+    W_conv1 = weight_variable([5,5,self.input_shape[2],32])
     b_conv1 = bias_variable([32])
 
     h_conv1 = tf.nn.relu(conv2d(x, W_conv1) + b_conv1)
@@ -47,10 +53,12 @@ class ConvModel(DQNModel):
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
 
-    W_fc1 = weight_variable([21*21*64, 1024])
+    # max_pool_2x2 decreases both image axes to half length
+    pool_size = ((self.resize_shape[0] / 4) ** 2) * 64
+    W_fc1 = weight_variable([pool_size, 1024])
     b_fc1 = bias_variable([1024])
 
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 21*21*64])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, pool_size])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     W_fc2 = weight_variable([1024, self.num_actions])
@@ -91,4 +99,6 @@ class ConvModel(DQNModel):
   def reshape_observation(self, observation):
     img = Image.fromarray(observation)
     img = img.resize(self.resize_shape)
+    if self.grayscale:
+      img = img.convert('L')
     return np.array(img)
