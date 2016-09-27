06e1ea2ab523fe7be2d1f7fd9f4d4c1a0768471b
Risto Vuorio <vuoristo@gmail.com>
2016-09-27 21:05:00 +0300

    CartPole updated to use window_size=4

--- CartPoleAgent.py
+++ CartPoleAgent.py
@@ -7,8 +7,8 @@ ENV_NAME = 'CartPole-v0'
 def main():
   env = gym.make(ENV_NAME)
   model = DNNModel(env, initial_learning_rate=0.1,
-                   gamma=0.99, hidden_1=10, hidden_2=6,
-                   soft_updates=True)
+                   gamma=0.99, hidden_1=256, hidden_2=16,
+                   soft_updates=True, window_size=4)
   agent = DQNAgent(env, model, epsilon=0.9,
                    epsilon_decay=0.97,
                    exp_buffer_size=40000, batch_size=50)
