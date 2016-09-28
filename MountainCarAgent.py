6b28e8d29f8671880c8b474871c5f1d2830bb49c
Risto Vuorio <vuoristo@gmail.com>
2016-09-28 11:11:49 +0300

    update MountainCarAgent

--- MountainCarAgent.py
+++ MountainCarAgent.py
@@ -1,15 +1,19 @@
 import gym
 
-from SimpleDQNModel import SimpleDQNModel
+from DNNModel import DNNModel
 from DQNAgent import DQNAgent
 
 ENV_NAME = 'MountainCar-v0'
 def main():
   env = gym.make(ENV_NAME)
-  while 1:
-    model = SimpleDQNModel(env, hidden_1=16, hidden_2=16, num_steps_per_decay=10000, soft_updates=True, gamma=0.999, tau=0.001, initial_learning_rate=0.01, learning_rate_decay_factor=0.95)
-    agent = DQNAgent(env, model, epsilon=0.1, min_epsilon=0.001, epsilon_decay=0.99, max_steps=2000, batch_size=50)
-    agent.train()
+  model = DNNModel(env, initial_learning_rate=0.01,
+                   gamma=0.99, hidden_1=256, hidden_2=16,
+                   soft_updates=True, window_size=4)
+  agent = DQNAgent(env, model, epsilon=0.1,
+                   epsilon_decay=0.97, min_epsilon=0.001,
+                   exp_buffer_size=40000, batch_size=50,
+                   max_steps=10000)
+  agent.train()
 
 if __name__ == '__main__':
   main()
