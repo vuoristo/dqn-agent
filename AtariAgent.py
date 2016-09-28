c60b26322aac0e487805a8d911caeaebabfb2a97
Risto Vuorio <vuoristo@gmail.com>
2016-09-28 15:39:10 +0300

    AtariAgent hyper parameters from Mnih paper

--- AtariAgent.py
+++ AtariAgent.py
@@ -6,9 +6,11 @@ from DQNAgent import DQNAgent
 ENV_NAME = 'Breakout-v0'
 def main():
   env = gym.make(ENV_NAME)
-  model = ConvModel(env, soft_updates=True)
-  agent = DQNAgent(env, model, linear_epsilon_decay=True, epsilon=1.0,
-                   min_epsilon=0.05)
+  model = ConvModel(env, learning_rate=2.5e-4, momentum=0.95, gamma=0.99,
+      tau=0.01, soft_updates=False)
+  agent = DQNAgent(env, model, linear_epsilon_decay=True,
+      epsilon_decay_steps=1.e6, epsilon=1.0, min_epsilon=0.1,
+      exp_buffer_size=1e6, batch_size=32)
   agent.train()
 
 if __name__ == '__main__':
